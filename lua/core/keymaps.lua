8dd2dc65c99cb923cd2e3c87bf65591380abd6ea
corsantic <aydofurkan25@gmail.com>
2025-08-22 13:12:04 +0300

    conflict-marker

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>mx", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
