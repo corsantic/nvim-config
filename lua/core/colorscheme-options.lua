306df7e9d7fa865e099f6a1fb4793b9837f536ca
corsantic <aydofurkan25@gmail.com>
2025-09-16 12:55:41 +0300

    onedark

--- lua/core/colorscheme-options.lua
+++ lua/core/colorscheme-options.lua
@@ -145,7 +145,49 @@ require('kanagawa').setup({
   },
 })
 
-vim.cmd("colorscheme kanagawa")
+-- Onedark
+-- default options
+require('onedark').setup {
+  -- Main options --
+  style = 'warmer',                 -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+  transparent = false,            -- Show/hide background
+  term_colors = true,             -- Change terminal color as per the selected theme style
+  ending_tildes = false,          -- Show the end-of-buffer tildes. By default they are hidden
+  cmp_itemkind_reverse = false,   -- reverse item kind highlights in cmp menu
+
+  -- toggle theme style ---
+  toggle_style_key = nil,                                                              -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+  toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+
+  -- Change code style ---
+  -- Options are italic, bold, underline, none
+  -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+  code_style = {
+    comments = 'italic',
+    keywords = 'none',
+    functions = 'bold',
+    strings = 'none',
+    variables = 'none'
+  },
+
+  -- Lualine options --
+  lualine = {
+    transparent = false,     -- lualine center bar transparency
+  },
+
+  -- Custom Highlights --
+  colors = {},       -- Override default colors
+  highlights = {},   -- Override highlight groups
+
+  -- Plugins Config --
+  diagnostics = {
+    darker = true,         -- darker colors for diagnostic
+    undercurl = true,      -- use undercurl instead of underline for diagnostics
+    background = true,     -- use background color for virtual text
+  },
+}
+vim.cmd("colorscheme onedark")
+-- vim.cmd("colorscheme kanagawa")
 
 -- vim.cmd("colorscheme rose-pine-main")
 -- vim.cmd("colorscheme rose-pine-moon")
@@ -161,3 +203,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     vim.fn.matchadd("AtStatement", [[@\%(if\|else\|elseif\|foreach\|for\|while\|endif\|endforeach\|endfor\|endwhile\)]])
   end,
 })
+
+
+
+-- one dark
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,25 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim",      name = "catppuccin", priority = 1000 },
   {
     "luisiacc/gruvbox-baby",
   },
-  {"rebelot/kanagawa.nvim"},
+  { "rebelot/kanagawa.nvim" },
   {
     "rose-pine/neovim",
     name = "rose-pine",
     config = function()
       vim.cmd("colorscheme rose-pine")
     end
+  },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
+      -- Enable theme
+      require('onedark').load()
+    end
   }
 }
-
