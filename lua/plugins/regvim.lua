30d91870fc29285a7997157012ad3967cf0f39bc
corsantic <aydofurkan25@gmail.com>
2025-09-16 23:08:05 +0300

    regvim config change

--- lua/plugins/regvim.lua
+++ lua/plugins/regvim.lua
@@ -1,10 +1,11 @@
 return {
   -- dir = "/home/enemo/project/regvim",
   "corsantic/regvim",
-  event = "CmdlineEnter",             -- Load when entering command line
+  event = "CmdlineEnter", -- Load when entering command line
   config = function()
     require("regvim").setup({
       convert_key = '<Tab>', -- Key to trigger manual conversion in command mode
+      escape_characters = { "(", ")", "+", "?", "|", "{", "}", "[", "]" },
     })
   end,
 }
