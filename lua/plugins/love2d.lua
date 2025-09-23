47f10b43579226cac8ed2c6ba0397c7908f38e11
corsantic <aydofurkan25@gmail.com>
2025-09-23 10:24:25 +0300

    love2d

--- lua/plugins/love2d.lua
+++ lua/plugins/love2d.lua
@@ -1,12 +1,16 @@
 return {
-  -- using lazy.nvim
-  "S1M0N38/love2d.nvim",
-  event = "VeryLazy",
-  version = "2.*",
-  opts = {},
-  keys = {
-    { "<leader>v",  ft = "lua",          desc = "LÖVE" },
-    { "<leader>vv", "<cmd>LoveRun<cr>",  ft = "lua",   desc = "Run LÖVE" },
-    { "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua",   desc = "Stop LÖVE" },
-  },
+	-- using lazy.nvim
+	"S1M0N38/love2d.nvim",
+	event = "VeryLazy",
+	version = "2.*",
+	opts = {
+		debug_window_opts = {
+			split = "below",
+		},
+	},
+	keys = {
+		{ "<leader>v", ft = "lua", desc = "LÖVE" },
+		{ "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+		{ "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+	},
 }
