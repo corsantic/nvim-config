077a6e994b15d074d81628c6be216ce2487759e1
corsantic <aydofurkan25@gmail.com>
2025-10-12 19:53:05 +0300

    noice options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -73,7 +73,7 @@ require("bufferline").setup({})
 -- telescope setup
 require("telescope").setup({
 	defaults = {
-		file_ignore_patterns = { "node_modules", "venv", "dist", "build" },
+		file_ignore_patterns = { "node_modules", "venv", "dist", "build", "%.png" },
 	},
 	extensions = {
 		["ui-select"] = {
@@ -251,26 +251,6 @@ end)
 -- autosession
 --
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-
--- noice setup
-require("noice").setup({
-	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-		},
-	},
-	-- you can enable a preset for easier configuration
-	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		command_palette = true, -- position the cmdline and popupmenu together
-		long_message_to_split = true, -- long messages will be sent to a split
-		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
-	},
-})
 
 require("notify").setup({
 	background_colour = "#000000",
