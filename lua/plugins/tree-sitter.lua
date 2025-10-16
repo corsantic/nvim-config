f7078b00054e7e941b528eabc27aede52a5867e5
corsantic <aydofurkan25@gmail.com>
2025-10-16 15:36:39 +0300

    .

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,31 +1,43 @@
 return {
-  { "nvim-treesitter/nvim-treesitter-refactor" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = { "OXY2DEV/markview.nvim" },
-    lazy = false,
-    branch = "master",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        ensure_installed =
-        {
-          "lua", "vim", "vimdoc", "query", "markdown",
-          "markdown_inline", "html", "json", "typescript", "zig", "c_sharp",
-          "elixir", "python", "heex", "http"
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        refactor = {
-          -- highlight_current_scope = { enable = true },
-          highlight_definitions = {
-            enable = true,
-            clear_on_cursor_move = true
-          },
-        },
-      }
-    end,
-  }
+	{ "nvim-treesitter/nvim-treesitter-refactor" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "OXY2DEV/markview.nvim" },
+		lazy = false,
+		branch = "master",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"html",
+					"json",
+					"typescript",
+					"zig",
+					"c_sharp",
+					"elixir",
+					"python",
+					"heex",
+					"http",
+					"dart",
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				refactor = {
+					-- highlight_current_scope = { enable = true },
+					highlight_definitions = {
+						enable = true,
+						clear_on_cursor_move = true,
+					},
+				},
+			})
+		end,
+	},
 }
