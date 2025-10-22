2c71ae3d05f97fbfc547617cb9fb9f884062c69b
corsantic <aydofurkan25@gmail.com>
2025-10-22 17:55:10 +0300

    .

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -1,35 +1,38 @@
 return {
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    requires = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {
-        filters = {
-          dotfiles = false, -- Show hidden files by default (change to true to hide)
-          custom = { "^.git$" }, -- Hide .git folder
-        },
-        renderer = {
-          group_empty = true,
-          icons = {
-            show = {
-              folder_arrow = true,
-            },
-          },
-        },
-        view = {
-          width = 30,
-        },
-      }
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		requires = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup({
+				filters = {
+					dotfiles = true,
+					git_ignored = true,
+				},
+				renderer = {
+					group_empty = true,
+					icons = {
+						show = {
+							folder_arrow = true,
+						},
+					},
+				},
+				view = {
+					width = 30,
+				},
+			})
 
-      -- Toggle hidden files with 'H' in nvim-tree
-      vim.keymap.set("n", "H", function()
-        require("nvim-tree.api").tree.toggle_hidden_filter()
-      end, { desc = "Toggle hidden files in nvim-tree", buffer = true })
-    end,
-  }
+			-- Toggle hidden files and gitignored files with 'H' and 'I' in nvim-tree
+			local api = require("nvim-tree.api")
+
+			-- You can use these keybindings inside nvim-tree:
+			-- Press 'H' to toggle dotfiles (hidden files like .env, .gitignore)
+			-- Press 'I' to toggle gitignored files (like node_modules, bin, obj)
+			-- Press 'U' to toggle both at once
+		end,
+	},
 }
