997b6917781fdbe666163eb166dc1ee7b49a58eb
corsantic <aydofurkan25@gmail.com>
2025-10-22 17:50:13 +0300

    H for unhiding in nvim-tree

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -8,7 +8,28 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     config = function()
-      require("nvim-tree").setup {}
+      require("nvim-tree").setup {
+        filters = {
+          dotfiles = false, -- Show hidden files by default (change to true to hide)
+          custom = { "^.git$" }, -- Hide .git folder
+        },
+        renderer = {
+          group_empty = true,
+          icons = {
+            show = {
+              folder_arrow = true,
+            },
+          },
+        },
+        view = {
+          width = 30,
+        },
+      }
+
+      -- Toggle hidden files with 'H' in nvim-tree
+      vim.keymap.set("n", "H", function()
+        require("nvim-tree.api").tree.toggle_hidden_filter()
+      end, { desc = "Toggle hidden files in nvim-tree", buffer = true })
     end,
   }
 }
